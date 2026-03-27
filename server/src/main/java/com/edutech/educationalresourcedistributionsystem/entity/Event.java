02f529af8f4d527a78d657d11faaa712f5d72448
Teja-yadav <dummy@gmail.com>
2026-03-27 04:33:16 +0000

    solve initial commit

---
+++ server/src/main/java/com/edutech/educationalresourcedistributionsystem/entity/Event.java
@@ -0,0 +1,10 @@
+package com.edutech.educationalresourcedistributionsystem.entity;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Table(name = "events") // do not change table name
+public class Event {
+    // implement entity
+}
---
+++ server/src/main/java/com/edutech/educationalresourcedistributionsystem/entity/EventRegistration.java
@@ -0,0 +1,11 @@
+package com.edutech.educationalresourcedistributionsystem.entity;
+
+
+import javax.persistence.*;
+
+@Table(name = "registrations") // do not change table name
+public class EventRegistration {
+
+   // implement entity
+}
+
